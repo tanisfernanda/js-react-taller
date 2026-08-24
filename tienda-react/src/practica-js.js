194e666423b79a24e3cb647f16a407993f5b51c5
Tania Trujillo <tanisfernanda.ans@gmail.com>
2026-08-24 09:02:11 -0500

    Reto 1

--- tienda-react/src/practica-js.js
+++ tienda-react/src/practica-js.js
@@ -38,10 +38,8 @@ console.log("Promedio:", calcularPromedio(4.0, 3.5, 5.0));
 
 /* 
  Pregunta de control:
-
 console.log(): imprime o muestra un valor en la consola del terminal 
 para que el desarrollador pueda verlo. No afecta la ejecución del código ni guarda el resultado.
-
 return: Es una instrucción dentro de una función que especifica qué valor devuelve 
 esa función al código que la llamó, permitiendo guardar ese resultado en una variable o usarlo en otra operación.
 */
